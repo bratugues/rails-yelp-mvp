453e0c8a16d156abbf4412cbae4fe3a8449733df
Erick Lobo <erickloboabreu@hotmail.com>
2025-08-27 17:11:12 -0300

    finish challenge, improve

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,18 @@
 class ReviewsController < ApplicationController
   def new
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+     redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_content
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-  post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/reviews/new", to: "reviews#new", as: :new_restaurant_review
-  post "restaurants/:id/reviews", to: "reviews#create", as: :restaurant_reviews
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # # get "restaurants/:id/reviews/new", to: "reviews#new", as: :new_restaurant_review
+  # post "restaurants/:id/reviews", to: "reviews#create", as: :restaurant_reviews
+
+  resources :restaurants do
+    resources :reviews, only: [:index, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "posts#index"
